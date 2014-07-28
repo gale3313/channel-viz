e3270aad71f55341ae314193429d5f53bcaf367a
gale3313 <gale3313@gmail.com>
2014-07-28 15:52:10 -0800

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= 'j6XrKz6Rpirx9H5eITh1l4xtWiZhUcUvNeM7sV8gCjqzt3lD', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [144136023], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'gale3313 app name', // Replaces Xively logo in the header
+		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
